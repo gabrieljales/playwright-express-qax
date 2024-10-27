8e95f16ae6967de236db68a9251804a7edd0c027
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2024-10-27 17:10:00 -0300

    feat: add informations about task already exists modal error

--- src/pages/mark-l/homePage.ts
+++ src/pages/mark-l/homePage.ts
@@ -1,4 +1,6 @@
 export const homePageSelectors = {
   NEW_TASK_INPUT_ID: '#newTask',
   CREATE_TASK_BUTTON: 'css=button >> text=Create',
+  TASK_ALREADY_EXISTS_MODAL_ERROR: '#swal2-html-container',
+  TASK_ALREADY_EXISTS_MODAL_ERROR_MESSAGE: 'Task already exists!',
 };
