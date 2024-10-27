b69ca0eadd1d2f416d59da8a983b474dce4e518a
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2024-10-27 17:11:33 -0300

    refactor: using verifyTaskAlreadyExistsError and create tasks in beforeEach

--- src/tests/web/mark-l/tasks.spec.ts
+++ src/tests/web/mark-l/tasks.spec.ts
@@ -2,14 +2,22 @@ import { test, expect } from '@playwright/test';
 import { WebTaskSteps } from '../../../steps/web/mark-l/webTaskSteps';
 import { faker } from '@faker-js/faker';
 import { ApiTaskSteps } from '../../../steps/api/mark-l/apiTaskSteps';
+import { CreateTask } from '../../../domain/entities/createTask';
+import { homePageSelectors } from "../../../pages/mark-l/homePage";
 
 test.describe('Testes da funcionalidade de tarefas', () => {
   let webTaskSteps: WebTaskSteps;
   let apiTaskSteps: ApiTaskSteps;
+  let newTask: CreateTask;
 
   test.beforeEach(async ({ page, request }, testInfo) => {
     webTaskSteps = new WebTaskSteps(page, testInfo);
     apiTaskSteps = new ApiTaskSteps(request);
+
+    newTask = {
+      name: faker.lorem.sentence(),
+      is_done: false
+    };
 
     await page.setViewportSize({ width: 1920, height: 1080 });
     await webTaskSteps.navigateToHomePage();
@@ -22,13 +30,11 @@ test.describe('Testes da funcionalidade de tarefas', () => {
 
   test('001 - Verificar o cadastro de uma nova tarefa via click no botão', async () => {
     // Dado que eu tenho uma nova tarefa
-    const taskName = faker.lorem.sentence();
-
     // Quando faço o cadastro dessa tarefa
-    await webTaskSteps.createTaskWithMouseClick(taskName);
+    await webTaskSteps.createTaskWithMouseClick(newTask.name);
 
     // Então essa tarefa deve ser exibida na lista
-    const createdTask = await webTaskSteps.getTaskItemByTestId('task-item', taskName);
+    const createdTask = await webTaskSteps.getTaskItemByTestId('task-item', newTask.name);
     await expect(createdTask).toBeVisible();
 
     // Captura de tela
@@ -37,13 +43,12 @@ test.describe('Testes da funcionalidade de tarefas', () => {
 
   test('002 - Verificar o cadastro de uma nova tarefa via teclado apertando enter', async () => {
     // Dado que eu tenho uma nova tarefa
-    const taskName = faker.lorem.sentence();
 
     // Quando faço o cadastro dessa tarefa
-    await webTaskSteps.createTaskWithKeyboard(taskName);
+    await webTaskSteps.createTaskWithKeyboard(newTask.name);
 
     // Então essa tarefa deve ser exibida na lista
-    const createdTask = await webTaskSteps.getTaskItemByTestId('task-item', taskName);
+    const createdTask = await webTaskSteps.getTaskItemByTestId('task-item', newTask.name);
     await expect(createdTask).toBeVisible();
 
     // Captura de tela
@@ -52,15 +57,13 @@ test.describe('Testes da funcionalidade de tarefas', () => {
 
   test('003 - Verificar mensagem de erro ao tentar cadastrar tarefas duplicadas', async ({ page }) => {
     // Dado que eu tenho uma nova tarefa
-    const taskName = faker.lorem.sentence();
 
-    // Quando faço o cadastro dessa tarefa duas vezes
-    await webTaskSteps.createTaskWithKeyboard(taskName);
-    await webTaskSteps.createTaskWithKeyboard(taskName);
+    // Quando tento fazer o cadastro dessa tarefa duas vezes
+    await webTaskSteps.createTaskWithKeyboard(newTask.name);
+    await webTaskSteps.createTaskWithKeyboard(newTask.name);
 
     // Então uma mensagem de erro deve ser exibida alertando que a tarefa já existe
-    const error = page.locator('#swal2-html-container');
-    await expect(error).toHaveText('Task already exists!');
+    await webTaskSteps.verifyTaskAlreadyExistsError();
 
     // Captura de tela
     await webTaskSteps.takeScreenshot(`task-already-exists_${Date.now()}`);
