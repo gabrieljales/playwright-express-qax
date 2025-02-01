5aee75200fbe003b0e2a3c8d9ff3c54c8d6c11da
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2025-02-01 15:00:24 -0300

    refactor: rename homePage to taskPageSelectors

--- src/pages/mark-l/taskPageSelectors.ts
+++ src/pages/mark-l/taskPageSelectors.ts
@@ -1,6 +1,7 @@
-export const homePageSelectors = {
+export const taskPageSelectors = {
   NEW_TASK_INPUT_ID: '#newTask',
   CREATE_TASK_BUTTON: 'css=button >> text=Create',
   TASK_ALREADY_EXISTS_MODAL_ERROR: '#swal2-html-container',
   TASK_ALREADY_EXISTS_MODAL_ERROR_MESSAGE: 'Task already exists!',
+  REQUIRED_FIELD_ERROR_MESSAGE: 'This is a required field',
 };
